30fe30523b20263adeb51d6e071f0a9009d6e710
hayasaka <hayasaka@jig.jp>
2026-08-06 15:35:16 +0900

    🔧 fix: Dartパッケージ名も改名に追従させる
    pubspec.yaml の name だけ旧名が残ると、import 文とリポジトリ名が食い違う。

--- samples/flutter/test/widget_test.dart
+++ samples/flutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:glasses_sdk_flutter_sample/main.dart';
+import 'package:sabera_app_sdk_flutter_sample/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

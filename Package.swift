cdfe762d31ed673e21b737d94504a111fd30f0b2
hayasaka <hayasaka@jig.jp>
2026-08-07 15:05:04 +0900

    🔧 fix: SDKを0.0.10に上げる

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/jig-jp/sabera-sdk/jp/jig/sabera/app/sdk/sabera-app-core-kmmbridge/0.0.8/sabera-app-core-kmmbridge-0.0.8.zip"
+let remoteKotlinUrl = "https://maven.pkg.github.com/jig-jp/sabera-sdk/jp/jig/sabera/app/sdk/sabera-app-core-kmmbridge/0.0.10/sabera-app-core-kmmbridge-0.0.10.zip"
 let remoteKotlinChecksum = "44ce24aeff87374711d117dbb68055ff86920ef10a43c504f5da9e7a14b70026"
 let packageName = "SaberaAppSDK"
 // END KMMBRIDGE BLOCK

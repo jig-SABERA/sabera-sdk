694ed4e405e4d3045be440baa835affbdb4de11f
hayasaka <hayasaka@jig.jp>
2026-08-19 09:40:56 +0900

    🆙 サンプルの依存を0.0.11に上げる
    APIリファレンスと同じバージョンで動かせるようにする。

--- samples/flutter/android/app/build.gradle.kts
+++ samples/flutter/android/app/build.gradle.kts
@@ -42,6 +42,6 @@ flutter {
 }
 
 dependencies {
-    implementation("jp.jig.sabera.app.sdk:sabera-app-core:0.0.10")
+    implementation("jp.jig.sabera.app.sdk:sabera-app-core:0.0.11")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 }
